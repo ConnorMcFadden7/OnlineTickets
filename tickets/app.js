309b1613e315e4cc4ecbfc4a78837d33805a15ab
Connor McFadden <connormcfadden7@gmail.com>
2014-07-27 21:17:59 +0100

    Admin panel reading data

---
+++ tickets/app.js
@@ -0,0 +1,40 @@
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('static-favicon');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var errorHandler = require('errorhandler');
+
+var routes = require('./routes/index');
+var admin  = require('./routes/admin');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorHandler());
+
+app.use('/', routes);
+app.use('/admin', admin);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+var server = app.listen(app.get('port'), function() {
+ 	console.log('Express server listening on port ' + server.address().port);
+});
---
+++ tickets/routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router  = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+/* Process the submit of the user ticket */
+router.post('/', function(req, res) {
+    res.render('index');
+});
+
+/* Show the thank you page after ticket is submitted */
+router.post('/thankyou', function(req, res) {
+    res.render('thankyou');
+
+    // Retreieve the information the user enters
+	var name = req.body.name;
+    var email = req.body.email;
+    var subject = req.body.subject;
+    var message = req.body.message;
+
+    // Connect to database
+    MongoClient.connect("mongodb://localhost:27017/tickets", function(err, db) {
+    	// Ensure we have connected
+    	if(err) {
+    		console.log("Cannot connect to database");
+    	} else {
+    		console.log("Connected to database");
+    	}
+    	// Create a collection to insert the tickets
+    	var collection = db.collection('tickets');
+        var ticketDocs = [{'name':name, 'email':email, 'subject':subject, 'message':message }];
+	    collection.insert(ticketDocs, {w:1}, function(err, data) {
+	    	// Check if the ticket has been inserted
+	    	if(err) {
+	    		console.log("There was a problem adding the information to the database.");
+	    	} else {
+	    		console.log("Added to database!" + name + " " + email);
+	    	}
+	    });
+	});
+});
+
+module.exports = router;
